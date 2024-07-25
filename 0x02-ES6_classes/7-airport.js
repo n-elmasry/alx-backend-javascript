2800425330281afd6c3e41915203e2af07030d5c
n-elmasry <almassrynada411@gmail.com>
2024-07-26 01:26:20 +0500

    task 7 8

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,15 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Override toString method to return the airport code
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
