edcfe35d104db8854f0c9faaba64a0a2a9782745
n-elmasry <almassrynada411@gmail.com>
2024-08-10 23:48:39 +0500

    task10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const value of array) {
-      const index = array.indexOf(value);
-      array[index] = appendString + value;
-    }
-
-    return array;
+  for (const value of array) {
+    const index = array.indexOf(value);
+    array[index] = appendString + value;
   }
+
+  return array;
+}
