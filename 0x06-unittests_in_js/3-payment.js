20839c5ec76896bacd2d865d40caf89ba266d8b1
n-elmasry <almassrynada411@gmail.com>
2024-10-03 21:53:43 +0500

    task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,9 +1,9 @@
 // function sendPaymentRequestToApi
-const Utils = require('./utils')
+const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    console.log('The total is: ', Utils.calculateNumber('SUM', totalAmount, totalShipping))
-
-}
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,19 +1,16 @@
-// utils.js
-
 const Utils = {
     calculateNumber(type, a, b) {
-        if (type === 'SUM') {
-            return Math.round(a) + Math.round(b);
-        } else if (type === 'SUBTRACT') {
-            return Math.round(a) - Math.round(b);
-        } else if (type === 'DIVIDE') {
-            if (Math.round(b) === 0) {
-                return 'Error';
-            } else {
-                return Math.round(a) / Math.round(b);
-            }
-        }
-    }
-};
+      if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+      }
+      if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+      }
+      if (type === 'DIVIDE') {
+        return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+      }
+      return 0;
+    },
+  };
 
-module.exports = Utils;
+  module.exports = Utils;
