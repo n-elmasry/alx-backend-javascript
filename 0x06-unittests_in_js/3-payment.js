9100bbd4b24e36273c7bb202e3de233dadd897c0
n-elmasry <almassrynada411@gmail.com>
2024-10-03 21:37:26 +0500

    task 3

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,9 @@
+// function sendPaymentRequestToApi
+const Utils = require('./utils')
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    console.log('The total is: ', Utils.calculateNumber('SUM', totalAmount, totalShipping))
+
+}
+
+module.exports = { sendPaymentRequestToApi };
---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,29 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const Utils = require('./utils');
+const { sendPaymentRequestToApi } = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function () {
+    let calculateNumberSpy;
+
+    beforeEach(function () {
+        // Create a spy for Utils.calculateNumber
+        calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    });
+
+    afterEach(function () {
+        // Restore the original method to prevent side effects
+        calculateNumberSpy.restore();
+    });
+
+    it('should call Utils.calculateNumber with correct arguments', function () {
+        // Call the function
+        sendPaymentRequestToApi(100, 20);
+
+        // Validate that the spy was called with the expected arguments
+        expect(calculateNumberSpy.calledOnce).to.be.true;
+        expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    });
+});
